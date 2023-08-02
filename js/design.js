313650de7cdc64f475a62eda89b922024ac53428
dumicode-hj103 <123072569+dumicode-hj103@users.noreply.github.com>
2023-08-02 21:59:48 +0800

    Add Page
    index
    1_브랜드관
    3_1_상품리스트
    3_2_상품리스트_아이템선택
    3_2_상품리스트_카테고리배너

--- js/design.js
+++ js/design.js
@@ -1,3 +1,58 @@
 $(function () {
-//
+  $(window).scroll(function () {
+
+  });
+
+  $('#btnTop').click(function () {
+    $("body, html").stop().animate({
+      scrollTop: 0
+    });
+    return false;
+  });
+
+  $('.category > li > a').click(function(){
+    $(this).parent().toggleClass('on');
+    return false;
+  });
+
+  $('.tabBox .tabTitle .itemTitle').click(function(){
+    $(this).parent().addClass('on').siblings().removeClass('on');
+    $(this).parents('.tabBox').find('.tabCont').eq($(this).parent().index()).addClass('on').siblings().removeClass('on');
+    return false;
+  });
+
+  $('#header .btnCate').click(function(){
+    $('body').toggleClass('openCateMenu');
+  });
 });
+
+function openPop(id) {
+  $('body').addClass('openPop');
+  $('#' + id).addClass('on');
+  return false;
+}
+
+function closePop(obj) {
+  $('body').removeClass('openPop');
+  $(obj).parents('.popBox').removeClass('on');
+  return false;
+}
+
+function closePopToday(obj) {
+  //cookie setting
+  //todo ....
+
+  closePop(obj);
+  return false;
+}
+
+function togglePop(id) {
+  $('#' + id).toggleClass('on');
+  return false;
+}
+
+function botBrandClick(obj) {
+  $(obj).toggleClass('on');
+  togglePop('popBrand');
+  $('body').toggleClass('openBotBrand');
+}
